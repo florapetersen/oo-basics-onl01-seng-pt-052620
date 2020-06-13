c147b4866b0c8b38595d1c54c28c3a6cf7080758
Flora Petersen <floramarina@comcast.net>
2020-06-13 02:33:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,26 +1,7 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   def initialize(brand)
     @brand = brand
-  end
-  def brand
-    @brand
-  end
-  def color=(color)
-    @color = color
-  end
-  def color
-    @color
-  end
-  def size=(size)
-    @size = size
-  end
-  def size
-    @size
-  end
-  def material=(material)
-    @material = material
-  end
-  def material
-    @material
   end
 end
